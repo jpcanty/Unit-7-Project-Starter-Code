e1e98475187b1361f06b830ba1d3ace518e78b4a
Jelani Canty <jelani.canty@gmail.com>
2022-05-22 23:50:09 -0500

    Created using Popcode: https://popcode.org

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 $("button").click(function(){
     let answer1 = $(".answer1").val();
-    let answer2 = $("change-me").val();
-    let answer3 = $("change-me").val();
-    alert(answer1);
+    let answer2 = $(".answer2").val();
+    let answer3 = $(".answer3").val();
+    $(".fortune-display").text("If you keep practicing " + answer3 + " for " + answer2 + " hours per week, you will be hired by " + answer1);
 });
